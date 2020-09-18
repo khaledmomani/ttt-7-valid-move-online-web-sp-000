11144c14a38cae20b644bf3e6fb96e78a2eeb7c2
Khaled S Momani <khaledmomani4@gmail.com>
2020-09-18 23:06:59 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -13,15 +13,10 @@ end
 end
 
 
-def valid_move?(board,index)
-  if  user_input.to_i > 0 && user_input.to_i < 10  && position_taken? == true
-    false
-  else
-     true
-  end
-  if position_taken? == false
-    true
-  else
-  false 
+def valid_move?(board, index)
+  if !position_taken?(board, index) && (index).between?(0,8)
+    return true
+  else 
+    return false
   end
 end
